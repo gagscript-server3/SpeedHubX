0a4862d1edd3caf01fd1d7c26da65835d623dc23
githubusercontent1.com <growtopiadontban1@gmail.com>
2025-08-21 11:51:34 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,2 @@
 ID="9a255ad5-2171-44ab-b2f3-b38d5e39dc51";
 loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
